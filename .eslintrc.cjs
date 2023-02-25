813b5970626b607c96f07efb8affd962b6903d50
WhitePaper <30739658+WhitePaper233@users.noreply.github.com>
2023-02-25 13:42:45 +0800

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'space-before-function-paren': 0,
+    'no-multiple-empty-lines': 0
+  }
+}
