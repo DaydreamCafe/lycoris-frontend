e8cf7e4b13595a3e375ea1d109a5e3042ace2604
White_Paper_ <1423634265@qq.com>
2023-02-26 20:23:10 +0800

    ✨ 控制台新增一个label

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+console.log("%c Lycoris | A project by DaydreamCafé " + " %c https://github.com/DaydreamCafe ", "color: white; background: #f466b2; padding:5px 0;", "padding:4px;border:1px solid #f466b2;");
+
 createApp(App).mount('#app')
